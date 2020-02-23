43162afd090eedf29f8bf142b90215bf45991192
PatEvs <ppevans11@gmail.com>
2020-02-23 13:45:02 +1300

    ✨  Created hello_hc06.ino

--- hc06/hc06.ino
+++ hc06/hc06.ino
@@ -6,24 +6,41 @@
 
 SoftwareSerial hc06(2,3);
 
+String cmd="";
+float sensor_val=0;
+
 void setup(){
     //Initialize Serial Monitor
     Serial.begin(9600);
-    Serial.println("ENTER AT Commands:");
     //Initialize Bluetooth Serial Port
     hc06.begin(9600);
 }
 
 void loop(){
-    //Write data from HC06 to Serial Monitor
-    if (hc06.available()){
-        Serial.write(hc06.read());
+    //Read data from HC06
+    while(hc06.available()>0){
+        cmd+=(char)hc06.read();
     }
+    //Select function with cmd
+    if(cmd!=""){
+        Serial.print("Command recieved : ");
+        Serial.println(cmd);
+        // We expect ON or OFF from bluetooth
+        if(cmd=="ON"){
+            Serial.println("Function is on");
+        } else if(cmd=="OFF"){
+            Serial.println("Function is off");
+        }else{
+            Serial.println("Function is off by default");
+        }
+        cmd=""; //reset cmd
+    }
+    // Simulate sensor measurement
+    sensor_val=(float)random(256); // random number between 0 and 255
 
-    //Write from Serial Monitor to HC06
-    if (Serial.available()){
-        hc06.write(Serial.read());
-    }
+    //Write sensor data to HC06
+    hc06.print(sensor_val);
+    delay(100);
 }
 
 /* EOF */
